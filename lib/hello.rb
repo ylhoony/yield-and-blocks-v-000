5fc0122357bfb8c79275b941cdcb8f32d37b7a5f
Hoon Lee <yl.hoony@gmail.com>
2017-11-01 18:54:36 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,7 +1,10 @@
 def hello_t(array)
-  # names.each do |name|
-  #   yield(name)
-  # end
+  i = 0
+ 
+  while i < array.length
+    yield array[i]
+    i = i + 1
+  end
 end
 
 # call your method here!
