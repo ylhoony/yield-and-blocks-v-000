84cd480a61af8046542795a66ce2818491410d29
Hoon Lee <yl.hoony@gmail.com>
2017-11-01 18:53:54 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -6,4 +6,8 @@ end
 
 # call your method here!
 
-# hello_t(names) { |name| puts "#{name.upcase}" }
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
